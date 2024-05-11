bddb0986e64f916fadacd1b65aa1651f19bc02cd
j.salas2016 <jsalas198016@gmail.com>
2024-05-11 00:48:56 -0400

    Done with Part One

--- app.js
+++ app.js
@@ -3,51 +3,50 @@
 // PART ONE Fizz Buzz
 
     //Loop through all numbers 1 to 100.
-//for (let i = 0; i < 101; i++){
-    //console.log(i);
-//}
+for (let i = 0; i < 101; i++){
+    console.log(i);
+}
 
     // if a number is divisble by 3, log "Fizz"
 
     // 1 through 15
-/*function divisbleNumber (divisor){
- console.log('Fizz');
+function divisbleNumber (divisor){
   for (let i = 0; i <15; i ++){
     if (i % divisor === 0){
-   //     console.log(i);
+     console.log(i);
     }
   }
 }
  const advisor = 3;
- divisbleNumber(advisor);  // logs 'Fizz' with the number equals to the divisble 3
+ divisbleNumber(advisor);  
+ console.log('Fizz'); // logs 'Fizz' with the number equals to the divisble 3
 
 
     // If a number is divisible by 5, log "Buzz"
-  /*  let divisble = 5;
+  let divisble = 5;
     let number = 16;
     for (let i = 0; i < number; i ++){
-        console.log('Buzz');
         if(i % divisble === 0){
-    //        console.log(i);
+       console.log(i);
         }
-    }           // logs 'Buzz' with the numbers equals to the divisible 5
-*/
+    }  
+    console.log('Buzz');    // logs 'Buzz' with the numbers equals to the divisible 5
+
 
         // If a number is divisble by both 3 and 5, log "Fizz Buzz"
-        /*
         const divisbleOne = 5;
         const divisbleTwo = 3;
         const str = "Fizz Buzz";
 
         for (let i = 0; i < 16; i ++){
             if (i % divisbleOne === 0 ){ // 5
-                console.log(i, "Fizz");
+                console.log(i);
             }
             else if(i % divisbleTwo === 0){ // 3
-                console.log(i, "Buzz");
+                console.log(i);
             }
         }
-        console.log(str); */
+        console.log(str); 
 
 
         // If a number is not divisble by either 3 or 5, log the numbers
